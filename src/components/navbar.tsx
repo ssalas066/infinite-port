3f09a83d8c6e1eb5511734c5df4ae4c7eb18e813
Sergio Salas <ssalas28@cnm.edu>
2023-07-04 16:45:12 -0600

    layout for nav, creating links to future components.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,17 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <header className='w-full px-32 py-8 font-medium'>
+            <nav>
+                <Link href='/'>Home</Link>
+                <Link href='/about'>About</Link>
+                <Link href='/projects'>Projects</Link>
+                <Link href='/articles'>Articles</Link>
+            </nav>
+            
+            </header>
     )
 }
 

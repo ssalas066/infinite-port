b6d96f5c685e624c53a953778594a5a55a1113c7
Sergio Salas <ssalas28@cnm.edu>
2023-07-04 16:15:41 -0600

    added new font to app and index tsx.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
+import { Jost } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+const jost = Jost({
+  subsets: ['latin'],
+  variable: '--font-jost'
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${jost.variable} font-jost`}>
+     <Component {...pageProps} />
+     </main>
+  )
 }

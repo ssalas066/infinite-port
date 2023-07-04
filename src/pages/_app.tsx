e28aa1b315f7bd9490cf47387a4f0e371beacf83
Sergio Salas <ssalas28@cnm.edu>
2023-07-04 16:44:22 -0600

    added navbar to _app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { Jost } from 'next/font/google'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Navbar from '@/components/navbar'
 
 const jost = Jost({
   subsets: ['latin'],
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <link rel='icon' href='/favicon.ico' />
     </Head>
     <main className={`${jost.variable} font-jost w-full min-h-screen`}>
+      <Navbar />
      <Component {...pageProps} />
      </main>
      </>

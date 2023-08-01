b5aea5e1f2bcb9e501398a372981acdb38ca67ac
Sergio Salas <ssalas28@cnm.edu>
2023-08-01 17:13:23 -0600

    added react icons for nav. working on creating functioning navbar.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,14 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false)
+    const [color, setColor] = useState('transparent')
+    const [textColor, setTextColor] = useState('black')
+
     return (
         <header className='w-full px-32 py-8 font-medium'>
             <nav>

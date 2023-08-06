0d286312337db6a1ce6d595b7edd6492b317565a
Sergio Salas <ssalas28@cnm.edu>
2023-08-06 15:40:01 -0600

    added transition effects for navbar, styling for links.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -27,15 +27,29 @@ const Navbar = () => {
     }, []);
 
     return (
-        <header className='w-full px-32 py-8 font-medium'>
-            <nav>
-                <Link href='/'>Home</Link>
-                <Link href='/about'>About</Link>
-                <Link href='/projects'>Projects</Link>
-                <Link href='/articles'>Articles</Link>
-            </nav>
+        <div style={{backgroundColor: `${color}`}} className=' fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+            <div className=' max-w-7xl m-auto lg:m-0 flex justify-between items-center p-2 text-black'>
+                <Link href='/'>
+                    <Image className=' block fill-current text-white h-15 w-15 mr-2 ml-2 lg:ml-10' src={} width={60} height={60} alt='logo'/>
+                </Link>
+                <ul style={{color: `${textColor}`}} className=' hidden sm:flex'>
+                    <li className=' p-4 no-underline hover:underline hover:text-purple-600 hover:bg-slate-500/20 rounded-md text-xl font-semibold'>
+                        <Link href='/'>Home</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
+        // <header className='w-full px-32 py-8 font-medium'>
+        //     <nav>
+        //         <Link href='/'>Home</Link>
+        //         <Link href='/about'>About</Link>
+        //         <Link href='/projects'>Projects</Link>
+        //         <Link href='/articles'>Articles</Link>
+        //     </nav>
             
-            </header>
+        //     </header>
     )
 }
 

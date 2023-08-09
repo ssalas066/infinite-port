476d42915d8ca7a5342bbd10806ec323468224d2
Sergio Salas <ssalas28@cnm.edu>
2023-08-09 15:38:58 -0600

    configured screen size, bg color, and font family.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 
 
-const { fontFamily } = require(`tailwindcss/defaultTheme`)
+// const { fontFamily } = require(`tailwindcss/defaultTheme`)
 
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    '/public/images/**/*.{jpg}',
+    // '/public/images/**/*.{jpg}',
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+      backgroundColor: {
+        'page': "#e2e8f0",
+      },
       fontFamily: {
-        sans: [`var(--font-jost)`, ...fontFamily.sans],
+        // sans: [`var(--font-jost)`, ...fontFamily.sans],
+        sans: ['var(--font-jost)'],
+        jost: ['Jost']
       },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      // backgroundImage: {
+        // 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        // 'gradient-conic':
+          // 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
     },
   },
   plugins: [],

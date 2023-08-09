bfa5c453d6542afb9e63651411e54814fb1f28f5
Sergio Salas <ssalas28@cnm.edu>
2023-08-09 15:52:55 -0600

    added footer, still need to add contact info.

---
+++ src/components/footer.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+
+const Footer = () => {
+    return (
+        <div className=' fixed bottom-0 bg-slate-600 text-indigo-100 p-2 w-screen z-50'>
+            <ul className=' flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12'>
+                <li>
+                    <Link>
+                    </Link>
+                </li>
+                <li>
+                    <Link>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Footer;

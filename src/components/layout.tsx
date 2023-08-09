025454708ffc94d2d4713d1d7abb610c8a868d9c
Sergio Salas <ssalas28@cnm.edu>
2023-08-09 15:48:25 -0600

    nearly done with layout component, working to add footer.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,2 +1,19 @@
 import React, { PropsWithChildren } from 'react';
 import Navbar from './navbar';
+// import  from './footer';
+
+const Layout = ({ children }: PropsWithChildren) => {
+    return (
+        <>
+        <Navbar />
+        <div className=' bg-page'>
+            <div className=' bg-hero bg-no-repeat bg-contain bg-fixed bg-center' id='bg'>
+                {children}
+            </div>
+        </div>
+        <Footer />
+        </>
+    )
+}
+
+export default Layout;

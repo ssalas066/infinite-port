e3eaf3fe94b479db7848d16c25d80e59b1757708
Sergio Salas <ssalas28@cnm.edu>
2023-08-09 16:23:50 -0600

    created footer, placeholder, will style differently, not fixed.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
         <div className=' fixed bottom-0 bg-slate-600 text-indigo-100 p-2 w-screen z-50'>
             <ul className=' flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12'>
                 <li>
-                    <Link>
-                    </Link>
+                    {/* <Link>
+                    </Link> */}
                 </li>
                 <li>
-                    <Link>
-                    </Link>
+                    {/* <Link>
+                    </Link> */}
                 </li>
             </ul>
         </div>

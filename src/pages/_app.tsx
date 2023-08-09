fc5ef1522a6b27b3b514b6fc5b0c3232bb0d30b5
Sergio Salas <ssalas28@cnm.edu>
2023-08-09 16:25:04 -0600

    added layout to page, new main with class name to style.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import '@/styles/globals.css'
 import { Jost } from 'next/font/google'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import Navbar from '@/components/navbar'
+// import Head from 'next/head'
+// import Navbar from '@/components/navbar'
+import Layout from '@/components/layout'
 
 const jost = Jost({
   subsets: ['latin'],
@@ -13,14 +14,17 @@ const jost = Jost({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
+    {/* <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <link rel='icon' href='/favicon.ico' />
-    </Head>
-    <main className={`${jost.variable} font-jost w-full min-h-screen`}>
-      <Navbar />
+    </Head> */}
+    {/* <main className={`${jost.variable} font-jost w-full min-h-screen`}> */}
+    <main className={`app ${jost.className}`}>
+      <Layout>
+      {/* <Navbar /> */}
      <Component {...pageProps} />
+     </Layout>
      </main>
      </>
-  )
+  );
 }

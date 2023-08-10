d73cffa27a9bfbe0c9eb86c2b7f1fc314fb9c90f
Sergio Salas <ssalas28@cnm.edu>
2023-08-10 15:48:00 -0600

    changed title to index.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import Hero from '@/components/hero';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Projects from '@/components/projects';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-      <title>InfiniteVoid Design</title>
+      <title>Infinite Void Design</title>
       <meta name='description' content='Infinite Solutions' />
     </Head>
     <Hero heading='I.V.D.' message='Infinite Solutions' />
+    <Projects />
     </div>
   );
 }

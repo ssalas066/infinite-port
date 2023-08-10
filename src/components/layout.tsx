8aefa80300dbac177b61c70ecfd770695397b34c
Sergio Salas <ssalas28@cnm.edu>
2023-08-10 14:35:46 -0600

    changed layout to void bg color, updated tailwind with void bg color name.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children }: PropsWithChildren) => {
     return (
         <>
         <Navbar />
-        <div className=' bg-page'>
+        <div className=' bg-void'>
             <div className=' bg-hero bg-no-repeat bg-contain bg-fixed bg-center' id='bg'>
                 {children}
             </div>

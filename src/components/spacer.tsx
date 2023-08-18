30b53e38766f0b23edfb7d806731cce2bdf0770d
Sergio Salas <ssalas28@cnm.edu>
2023-08-18 16:49:18 -0600

    new spacer const, added style change to global.

---
+++ src/components/spacer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+const Spacer = () => {
+    return (
+        <div className='bg-night'></div>
+    )
+}
+
+export default Spacer;
